2098de18111554339e90c0dcc3f36dc64f2fd96e
sunsetsavorer <sunset.savorer@gmail.com>
2025-09-08 23:39:46 +0300

    refactor: parallelized tasks

--- signer.go
+++ signer.go
@@ -60,9 +60,28 @@ var SingleHash = func(in, out chan interface{}) {
 
 		data := strconv.Itoa(v.(int))
 
-		crc := DataSignerCrc32(data)
-		md5 := DataSignerMd5(data)
-		crcFromMd5 := DataSignerCrc32(md5)
+		wg := &sync.WaitGroup{}
+
+		crc := ""
+		md5 := ""
+		crcFromMd5 := ""
+
+		wg.Add(2)
+
+		go func() {
+			md5 = DataSignerMd5(data)
+			crcFromMd5 = DataSignerCrc32(md5)
+
+			wg.Done()
+		}()
+
+		go func() {
+			crc = DataSignerCrc32(data)
+
+			wg.Done()
+		}()
+
+		wg.Wait()
 
 		res := crc + "~" + crcFromMd5
 
@@ -75,12 +94,34 @@ var MultiHash = func(in, out chan interface{}) {
 	for v := range in {
 
 		data := v.(string)
-		res := ""
+
+		mu := &sync.Mutex{}
+		wg := &sync.WaitGroup{}
+
+		m := make(map[int]string)
 
 		for th := 0; th <= 5; th++ {
 
-			crc := DataSignerCrc32(strconv.Itoa(th) + data)
-			res += crc
+			wg.Add(1)
+
+			go func(idx int) {
+
+				crc := DataSignerCrc32(strconv.Itoa(idx) + data)
+
+				mu.Lock()
+				m[idx] = crc
+				mu.Unlock()
+
+				wg.Done()
+			}(th)
+		}
+
+		wg.Wait()
+
+		res := ""
+
+		for i := 0; i <= 5; i++ {
+			res += m[i]
 		}
 
 		out <- res
